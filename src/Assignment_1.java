d3b4e32cdb9a6e440bc98ec6be107ac2d5b4c4b6
abhigyan-pandey <abhigyan.pandey@zemosolabs.com>
2021-07-19 00:20:39 +0530

    Completion of assignment-1

--- src/Assignment_1.java
+++ src/Assignment_1.java
@@ -1,4 +1,33 @@
+//  author : Abhigyan_Pandey
+//  Zemoso Java Asssignment
+
+import java.util.* ;
+import java.io.* ;
+import edu.duke.* ;
 public class Assignment_1
 {
-    
+    public void searchForFile(String regexpression , String fileName)
+    {
+            FileResource fr = new FileResource(fileName) ;    // Opening the file with specified name
+            for(String line : fr.lines())
+            {
+                if(line.contains(regexpression) )   // Printing the file name if regular expression is found
+                    System.out.println("The file with respective expression is found and the file name is -> " + fileName);
+
+                else
+                    System.out.println("This file does'nt contain the required expression");
+            }
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in) ;
+        String regularExpression , fileName ;
+        regularExpression = sc.nextLine() ;
+        fileName = sc.nextLine() ;
+        Assignment_1 object = new Assignment_1() ;
+        while(true)
+        {
+            object.searchForFile(regularExpression,fileName);   // Calling the function for the purpose.
+        }
+    }
 }
